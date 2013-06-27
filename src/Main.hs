8ce0e7a116221acd5e536b88ae0d0d1ee4f33d16
aarond_1 <aarond+1@fpcomplete.com>
2013-06-27 07:45:53 +0000

    1st commit

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,20 @@
-module Main where
+{-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses,
+             TemplateHaskell, OverloadedStrings #-}
+import Yesod
 
-main = putStrLn "Welcome to the FP Haskell Center!"
+data Piggies = Piggies
+
+instance Yesod Piggies
+
+mkYesod "Piggies" [parseRoutes|
+  / HomeR GET
+|]
+-- test  here
+getHomeR = defaultLayout [whamlet|
+  The web piggies are here on aarond_1!  For a second time
+  <br>
+  <a href="http://fpcomplete.com">Visit our site!
+  <p>
+  |]
+
+main = warpEnv Piggies
